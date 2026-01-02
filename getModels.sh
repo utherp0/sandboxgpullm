8efc4476c10e7bf3fa7fd380d40ddcd2a93d3f10
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2026-01-02 15:56:11 +0000

    Update getModels.sh

--- getModels.sh
+++ getModels.sh
@@ -1,5 +1,5 @@
 for hostService in $(oc get isvc -n sandbox-shared-models -o jsonpath='{.items[*].status.address.url}');
-do echo $hostService:
+do echo $hostService
    curl -skL -H "Authorization: Bearer "$(oc whoami -t) $hostService/v1/models | jq -r '.data[].id';
    printf "\n\r"
 done
